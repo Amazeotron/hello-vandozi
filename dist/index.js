cc0d05617437d54776483a43a991c4badb1b1974
larry-sudozi <larry@sudozi.com>
2025-11-06 22:00:28 -0800

    remove use strict

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,8 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 // import dotenv from "dotenv";
@@ -11,5 +12,5 @@ const app = (0, express_1.default)();
 app.get("/surf-conditions", surf_conditions_1.surfConditions);
 const port = parseInt(process.env.PORT || "8080", 10);
 app.listen(port, () => {
-    console.log(`Vandozi: listening on port ${port}`);
+  console.log(`Vandozi: listening on port ${port}`);
 });
