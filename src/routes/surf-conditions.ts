94140792acec6eacf2f2a0b0d0437fd29725f0b7
larry-sudozi <larry@sudozi.com>
2025-11-07 22:09:23 -0800

    tsend text to browser whn its html

--- src/routes/surf-conditions.ts
+++ src/routes/surf-conditions.ts
@@ -2,55 +2,64 @@ import { synthesizeText } from "@src/utils/synthesize-text.js";
 import type { Request, Response } from "express";
 import fetch from "node-fetch";
 
-export const surfConditions = async (req: Request, res: Response) => {
-  try {
-    const response = await fetch(
-      "https://services.surfline.com/kbyg/regions/forecasts/conditions?subregionId=5cc73566c30e4c0001096989&days=1&accesstoken=b892cc4f756bdbce41c7abfd05f96cae384664fd"
-    );
-    const surfData = (await response.json()) as {
-      data: { conditions: { observation: string }[] };
+type SurfDataType = {
+  data: { conditions: { observation: string }[] };
+};
+
+const fetchSurfReport = async () => {
+  const response = await fetch(
+    "https://services.surfline.com/kbyg/regions/forecasts/conditions?subregionId=5cc73566c30e4c0001096989&days=1&accesstoken=b892cc4f756bdbce41c7abfd05f96cae384664fd"
+  );
+  // Test to see if the repsonse is html or json
+  const contentType = response.headers.get("content-type");
+  const responseText = await response.clone().text();
+  if (
+    (contentType && contentType.includes("text/html")) ||
+    responseText.startsWith("<")
+  ) {
+    return {
+      type: "html",
+      body: responseText,
     };
-    const { observation } = surfData.data.conditions[0];
+  } else {
+    const json = (await response.json()) as SurfDataType;
+    const { observation } = json.data.conditions[0];
     // remove any <br> tags or \n characters
     const cleanedObservation = observation
       .replace(/<br\s*\/?>/gi, " ")
       .replace(/\n+/g, " ")
       .trim();
+    return {
+      type: "text",
+      body: cleanedObservation,
+    };
+  }
+};
 
-    const maybeAudioFilePath = await synthesizeText(cleanedObservation);
+export const surfConditions = async (req: Request, res: Response) => {
+  const surfData = await fetchSurfReport();
+  if (surfData.type === "html") {
+    // send the html response as is
+    res.status(200).send(surfData.body);
+    return;
+  }
 
-    if (
-      maybeAudioFilePath &&
-      typeof maybeAudioFilePath === "object" &&
-      "error" in maybeAudioFilePath
-    ) {
-      res.status(500).send(maybeAudioFilePath.error);
-    } else if (typeof maybeAudioFilePath === "string") {
-      res.setHeader("Content-Type", "audio/mpeg");
-      res.sendFile(maybeAudioFilePath, (err) => {
-        if (err) {
-          res.status(500).send(err);
-        }
-      });
-    } else {
-      res.status(500).send("Unexpected error occurred.");
-    }
-  } catch (err) {
-    console.error(err);
-    const audioFilePath = await synthesizeText(
-      "Sorry, we couldn't retrieve the surf conditions."
-    );
+  const maybeAudioFilePath = await synthesizeText(surfData.body);
 
-    if (typeof audioFilePath !== "string") {
-      res.status(500).send("Unexpected error occurred.");
-      return;
-    }
+  if (
+    maybeAudioFilePath &&
+    typeof maybeAudioFilePath === "object" &&
+    "error" in maybeAudioFilePath
+  ) {
+    res.status(500).send(maybeAudioFilePath.error);
+  } else if (typeof maybeAudioFilePath === "string") {
     res.setHeader("Content-Type", "audio/mpeg");
-    res.sendFile(audioFilePath, (err) => {
+    res.sendFile(maybeAudioFilePath, (err) => {
       if (err) {
         res.status(500).send(err);
       }
     });
-    return;
+  } else {
+    res.status(500).send("Unexpected error occurred.");
   }
 };
