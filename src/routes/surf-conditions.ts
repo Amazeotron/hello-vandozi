0a7b92ff7f97c38dbcad2c095a0b099f10a69948
larry-sudozi <larry@sudozi.com>
2025-11-11 19:48:51 -0800

    trying elevnlabs

--- src/routes/surf-conditions.ts
+++ src/routes/surf-conditions.ts
@@ -7,33 +7,51 @@ type SurfDataType = {
 
 const pageUrl =
   "https://www.surfline.com/surf-report/linda-mar-north/5cbf8d85e7b15800014909e8";
-const url =
+const jsonUrl =
   "https://services.surfline.com/kbyg/regions/forecasts/conditions?subregionId=5cc73566c30e4c0001096989&days=1&accesstoken=b892cc4f756bdbce41c7abfd05f96cae384664fd";
 
 const endpoint = "https://production-sfo.browserless.io/chromium/bql";
-const token = "2TOpaSCtaEcRez7717502587ee2a117f0f2d859baba3ec452";
 
 export async function fetchSurfReport() {
-  const response = await fetch(`${endpoint}?token=${token}`, {
-    method: "POST",
+  // First try and just load the JSON
+  const jsonReport = await fetch(jsonUrl, {
+    method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      query: `
+  });
+  const jsonData = (await jsonReport.json()) as SurfDataType;
+  const observation = jsonData.data.conditions[0]?.observation;
+  if (observation) {
+    return {
+      type: "text",
+      body: `The current surf conditions at Linda Mar North are: ${observation}`,
+    };
+  }
+
+  const response = await fetch(
+    `${endpoint}?token=${process.env.BROWSERLESS_TOKEN}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
       mutation GetSurfReport($url: String!) {
         goto(url: $url, waitUntil: networkIdle) {
           status
         }
-        selector: text(selector: "[class^='SpotCurrentConditions_currentConditionsReportContainer']") {
+        selector: text(selector: "[class^='CurrentWaveRating_rating']") {
           text
         }
       }`,
-      variables: {
-        url: pageUrl,
-      },
-    }),
-  });
+        variables: {
+          url: pageUrl,
+        },
+      }),
+    }
+  );
 
   const data = (await response.json()) as {
     data: {
@@ -51,40 +69,49 @@ export async function fetchSurfReport() {
 }
 
 export const surfConditions = async (req: Request, res: Response) => {
-  try {
-    console.log("Fetching surf report...");
-    const surfData = await fetchSurfReport();
-    console.log("Surfdata:", surfData.body);
-    if (surfData.type === "html") {
-      // send the html response as is
-      res.status(200).send(surfData.body);
-      return;
-    }
-
-    const maybeAudioFilePath = await synthesizeText(surfData.body);
-
-    if (
-      maybeAudioFilePath &&
-      typeof maybeAudioFilePath === "object" &&
-      "error" in maybeAudioFilePath
-    ) {
-      res.status(500).send(maybeAudioFilePath.error);
-    } else if (typeof maybeAudioFilePath === "string") {
-      res.setHeader("Content-Type", "audio/mpeg");
-      res.sendFile(maybeAudioFilePath, (err) => {
-        if (err) {
-          res.status(500).send(err);
-        }
-      });
-    } else {
-      res.status(500).send("Unexpected error occurred.");
-    }
-  } catch (err) {
-    const errErr = err as Error;
-    console.log("Caught error in surfConditions:", errErr);
-    res
-      .status(500)
-      .send("message" in errErr ? errErr.message : "Unknown error");
-    console.error(errErr.stack);
+  const surfData = await fetchSurfReport();
+  if (surfData.type === "html") {
+    // send the html response as is
+    res.status(200).send(surfData.body);
+    return;
   }
+  const audio = await synthesizeText(surfData.body);
+  res.setHeader("Content-Type", "audio/mpeg");
+  res.send(audio);
+  // try {
+  //   console.log("Fetching surf report...");
+  //   const surfData = await fetchSurfReport();
+  //   console.log("Surfdata:", surfData.body);
+  //   if (surfData.type === "html") {
+  //     // send the html response as is
+  //     res.status(200).send(surfData.body);
+  //     return;
+  //   }
+
+  //   const maybeAudioFilePath = await synthesizeText(surfData.body);
+
+  //   if (
+  //     maybeAudioFilePath &&
+  //     typeof maybeAudioFilePath === "object" &&
+  //     "error" in maybeAudioFilePath
+  //   ) {
+  //     res.status(500).send(maybeAudioFilePath.error);
+  //   } else if (typeof maybeAudioFilePath === "string") {
+  //     res.setHeader("Content-Type", "audio/mpeg");
+  //     res.sendFile(maybeAudioFilePath, (err) => {
+  //       if (err) {
+  //         res.status(500).send(err);
+  //       }
+  //     });
+  //   } else {
+  //     res.status(500).send("Unexpected error occurred.");
+  //   }
+  // } catch (err) {
+  //   const errErr = err as Error;
+  //   console.log("Caught error in surfConditions:", errErr);
+  //   res
+  //     .status(500)
+  //     .send("message" in errErr ? errErr.message : "Unknown error");
+  //   console.error(errErr.stack);
+  // }
 };
--- src/utils/synthesize-text.ts
+++ src/utils/synthesize-text.ts
@@ -1,23 +1,53 @@
-import textToSpeech from "@google-cloud/text-to-speech";
-import fs from "fs";
-import path from "path";
-import util from "util";
+import { ElevenLabsClient } from "@elevenlabs/elevenlabs-js";
+import { createWriteStream } from "fs";
+import { Readable } from "stream";
+import { v4 as uuid } from "uuid";
+// import textToSpeech from "@google-cloud/text-to-speech";
+// import fs from "fs";
+// import path from "path";
+// import util from "util";
 
 export async function synthesizeText(text: string, fileName = "output.mp3") {
-  const client = new textToSpeech.TextToSpeechClient();
-  const request = {
-    input: { text: text },
-    voice: { languageCode: "en-US", ssmlGender: "NEUTRAL" as const },
-    audioConfig: { audioEncoding: "MP3" as const },
-  };
-  const dir = path.resolve("./");
-  const audioFilePath = path.join(dir, fileName);
+  const elevenlabs = new ElevenLabsClient({
+    apiKey: process.env.ELEVENLABS_API_KEY, // Defaults to process.env.ELEVENLABS_API_KEY
+  });
 
-  const [response] = await client.synthesizeSpeech(request);
-  if (!response.audioContent) {
-    return { error: "No audio content received from Text-to-Speech API" };
+  const audioStream = await elevenlabs.textToSpeech.stream(
+    "mgpcWiEXIWuENJCy8ADX", // voice_id
+    {
+      text,
+      modelId: "eleven_multilingual_v2",
+      outputFormat: "mp3_44100_128", // output_format
+      voiceSettings: {
+        stability: 0,
+        similarityBoost: 0,
+        useSpeakerBoost: true,
+        speed: 1.0,
+      },
+    }
+  );
+
+  const chunks: Uint8Array[] = [];
+  for await (const chunk of audioStream) {
+    chunks.push(chunk);
   }
-  const writeFile = util.promisify(fs.writeFile);
-  await writeFile(audioFilePath, response.audioContent, "binary");
-  return audioFilePath;
+  const content = Buffer.concat(chunks);
+  return content;
+
+  // const client = new textToSpeech.TextToSpeechClient();
+  // const request = {
+  //   input: { text: text },
+  //   voice: { languageCode: "en-US", ssmlGender: "NEUTRAL" as const },
+  //   audioConfig: { audioEncoding: "MP3" as const },
+  // };
+  // const dir = path.resolve("./");
+  // const audioFilePath = path.join(dir, fileName);
+
+  // const [response] = await client.synthesizeSpeech(request);
+  // if (!response.audioContent) {
+  //   return { error: "No audio content received from Text-to-Speech API" };
+  // }
+  // const writeFile = util.promisify(fs.writeFile);
+  // await writeFile(audioFilePath, response.audioContent, "binary");
+  // return audioFilePath;
 }
